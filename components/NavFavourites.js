3f92a777703cf7e9e4e5326cb2b0e821e01341ca
Leo Black <leonardoblack.net@gmail.com>
2022-02-07 21:32:35 -0300

    add favourite functionality

--- components/NavFavourites.js
+++ components/NavFavourites.js
@@ -1,26 +1,33 @@
 import React from 'react';
 
-import {StyleSheet, Text, View, FlatList, TouchableOpacity} from 'react-native';
+import {Text, View, FlatList, TouchableOpacity} from 'react-native';
 import {Icon} from "react-native-elements";
 import tw from "twrnc";
+import {useDispatch} from "react-redux";
+import {setDestination} from "../slices/navSlice";
 
 const data = [
     {
         id: '123',
         icon: 'home',
         location: 'Home',
-        destination: 'Code Street, London, Uk'
+        coords: {lat: -23.44279422829955, lng: -46.52444681884378},
+        destination: 'Rua Joao Artoni Testae'
     },
     {
         id: '456',
         icon: 'briefcase',
-        location: 'Work',
-        destination: 'London Eye, London, Uk'
+        location: 'Casa Amanda',
+        coords: {lat: -23.43116034660059, lng: -46.50586785782914},
+        destination: 'Rua Trajano de Faria'
     }
 ]
 
 
 const NavFavourites = () => {
+
+    const dispatch = useDispatch();
+
     return (
         <FlatList
             data={data}
@@ -33,8 +40,18 @@ const NavFavourites = () => {
                     ]}
                 />
             )}
-            renderItem={({item: {location, destination, icon}}) => (
-                <TouchableOpacity style={tw`flex-row items-center p-5`}>
+            renderItem={({item: {location, destination, icon, coords}}) => (
+                <TouchableOpacity
+                    style={tw`flex-row items-center p-5`}
+                    onPress={() => {
+                        dispatch(
+                            setDestination({
+                                location: coords,
+                                description: destination
+                            })
+                        )
+                    }}
+                >
                     <Icon
                         style={tw`mr-4 rounded-full bg-gray-300 p-3`}
                         name={icon}
